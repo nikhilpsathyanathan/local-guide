4e0405364fdb44febc7aeb93b7f2cca953758766
Nikhil P S <nikhilpsathyanathan@gmail.com>
2023-10-31 14:18:05 +0530

    chore: update next config to support images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "withlocals-com-res.cloudinary.com",
+        port: "",
+        pathname: "/image/upload/**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
